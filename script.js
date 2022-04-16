ff8969d778ee103bca04d87fe4de0a7467311f85
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2022-04-16 17:10:45 +0300

    fix

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-const countryInput = document.querySelector('.CountryCodeInput')
-console.log(countryInput)
+const countryInputContainer = document.querySelector('.CountryCodeInput')
+const countryNameInput = countryInputContainer.querySelector('input')
+const countryPhoneInput = document.getElementById('sign-in-phone-number')
+ countryNameInput.value = 'Russian Federation'
+countryPhoneInput.value = '+7 '
 
-countryInput.addEventListener('click', event => {
-    event.stopPropagation()
+console.log(countryInputContainer)
+
+function toggleMenu() {
     const countryMenu = document.querySelector('.bubble')
     countryMenu.classList.toggle('shown')
     countryMenu.classList.toggle('open')
+}
+
+function handleCountryInputClick (event)  {
+    event.stopPropagation()
+    toggleMenu()
+}
+
+countryInputContainer.addEventListener('click', handleCountryInputClick )
+
+const countryItems = document.querySelectorAll('.MenuItem')
+
+countryItems.forEach(countryItem => {
+    countryItem.addEventListener('click', event => {
+        const countryName = countryItem.querySelector('.country-name').innerText
+        const countryCode = countryItem.querySelector('.country-code').innerText
+
+        countryNameInput.value = countryName
+
+        countryPhoneInput.value = `${countryCode} `
+    })
 })
