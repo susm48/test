fe044ed850451f9081ed1c99edb6b72b5089b64f
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2022-05-02 23:15:53 +0300

    add next button dissapearing & spaces for russia

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const countryInputContainer = document.querySelector('.CountryCodeInput')
 const countryNameInput = countryInputContainer.querySelector('input')
 const countryPhoneInput = document.getElementById('sign-in-phone-number')
+const nextButton = document.getElementById('next-button')
  countryNameInput.value = 'Russian Federation'
 countryPhoneInput.value = '+7 '
 
@@ -30,3 +31,18 @@ countryItems.forEach(countryItem => {
         countryPhoneInput.value = `${countryCode} `
     })
 })
+
+countryPhoneInput.addEventListener('keydown', event => {
+    const length = event.target.value.length
+    const value = event.target.value
+    if (length > 8) {
+        nextButton.classList.remove('hidden-button')
+    } else {
+        nextButton.classList.add('hidden-button')
+    }
+    if (value.startsWith('+7') && event.code !== 'Backspace' && (length === 2 || length === 6 || length === 10)  ) {
+        console.log('lol')
+        countryPhoneInput.value = event.target.value + ' '
+    }
+})
+
